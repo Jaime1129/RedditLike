48e3258f08b28d533fd9d463a726834cb40602bb
Jaime1129 <jaime1129@163.com>
2018-07-02 11:19:39 +0800

    VoteForTopic

--- RedditLike/src/app/topic/topic.component.ts
+++ RedditLike/src/app/topic/topic.component.ts
@@ -3,11 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
-  styleUrls: ['./topic.component.css']
+  styleUrls: ['./topic.component.css'],
+  host: {
+    class: 'row'
+  }
 })
 export class TopicComponent implements OnInit {
 
-  constructor() { }
+  title: string; // less than 255 characters
+  likes: number; // upvotes minus downvotes
+
+
+  constructor() {
+    this.title = 'RedditTopic';
+    this.likes = 0;
+   }
+
+  upvote() {
+    this.likes++;
+  }
+
+  downvote() {
+    this.likes--;
+  }
 
   ngOnInit() {
   }
